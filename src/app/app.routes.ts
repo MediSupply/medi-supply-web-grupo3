05051bcff850d0b7427d70c04915015073a268c2
Santiago <s.patino@uniandes.edu.co>
2025-10-18 12:21:48 -0500

    signUp: Diseño y logica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegistrarUsuarioComponent } from './pages/registrar-usuario/registrar-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'registro', pathMatch: 'full' },
+    { path: 'registro', component: RegistrarUsuarioComponent }
+];
