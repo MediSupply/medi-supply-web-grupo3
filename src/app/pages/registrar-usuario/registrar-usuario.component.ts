1689be950a2adc98984b7d11147b5033bceb8c28
Santiago <s.patino@uniandes.edu.co>
2025-10-18 12:21:48 -0500

    signUp: Diseño y logica

---
+++ src/app/pages/registrar-usuario/registrar-usuario.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-registrar-usuario',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './registrar-usuario.component.html',
+  styleUrl: './registrar-usuario.component.scss'
+})
+export class RegistrarUsuarioComponent {
+  user = {
+    name: 'Juan Pérez',
+    email: 'juan@ejemplo.com',
+    password: 'password123',
+    role: 'USER',
+  };
+
+  loginData = {
+    correo: '',
+    password: ''
+  };
+
+  mensaje = '';
+  cargando = false;
+
+  constructor(private authService: AuthService) {}
+
+  registrarUsuario(form: NgForm) {
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.user.email);
+    if (!emailValido) {
+      this.mensaje = 'El correo electrónico no tiene un formato válido ⚠️';
+      return;
+    }
+
+    const passwordValida = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(this.user.password);
+    if (!passwordValida) {
+      this.mensaje = 'La contraseña debe tener al menos 8 caracteres e incluir letras y números ⚠️';
+      return;
+    }
+
+    if (form.invalid) {
+      this.mensaje = 'Por favor completa/valida todos los campos obligatorios ⚠️';
+      return;
+    }
+
+    this.cargando = true;
+    this.mensaje = '';
+
+    this.authService.signup(this.user).subscribe({
+      next: (response: any) => {
+        this.cargando = false;
+        this.mensaje = 'Usuario registrado correctamente ✅';
+        console.log('Respuesta del servidor:', response);
+
+        // Limpia el formulario después del registro
+        // this.user = { name: '', email: '', password: '', role: 'USER' };
+      },
+      error: (err: any) => {
+        this.cargando = false;
+
+        if (err.status === 409) {
+          this.mensaje = 'El correo electrónico ya está registrado ❌';
+        } else {
+          this.mensaje = 'Error al registrar el usuario ❌';
+        }
+        
+        console.error('Error:', err);
+      }
+    });
+  }
+
+  iniciarSesion() {
+    this.cargando = true;
+    this.mensaje = '';
+
+    this.authService.login(this.loginData.correo, this.loginData.password).subscribe({
+      next: (response:any) => {
+        this.cargando = false;
+        this.mensaje = 'Inicio de sesión exitoso ✅';
+        console.log('Respuesta del servidor:', response);
+      },
+      error: (err:any) => {
+        this.cargando = false;
+        this.mensaje = 'Error al iniciar sesión ❌';
+        console.error('Error:', err);
+      }
+    });
+  }
+
+}
