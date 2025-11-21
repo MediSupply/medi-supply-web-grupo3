ce726cf77cd30514cb9086894e761ac7c4dcfef5
Santiago <s.patino@uniandes.edu.co>
2025-11-20 21:50:33 -0500

    consumo registro proveedor service

--- src/app/modules/producto/producto.module.ts
+++ src/app/modules/producto/producto.module.ts
@@ -3,15 +3,8 @@ import { CommonModule } from '@angular/common';
 import { CargarProductoComponent } from './components/cargar-producto/cargar-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
-
-
 @NgModule({
-  declarations: [
-    CargarProductoComponent,
-    ListarProductosComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CargarProductoComponent, ListarProductosComponent],
+  imports: [CommonModule],
 })
-export class ProductoModule { }
+export class ProductoModule {}
